6c2d898c6cc6994208b0a0750430e30162aa926a
isjustomi <20230441@ricaldone.edu.sv>
2025-08-29 10:29:21 -0600

    Mis cambios

--- backend/app.js
+++ backend/app.js
@@ -21,13 +21,16 @@ const app = express();
 // Que acepte cookies
 app.use(cookieParser());
 
+// Que se parseen los campos
+app.use(express.json());
+
 // Definir las rutas de las funciones que tendrá la página web
-app.use("/api/products", validateAuthToken(["admin"]), productsRoutes);
+app.use("/api/products", validateAuthToken(["Admin"]), productsRoutes);
 app.use("/api/customers", customersRoutes);
 app.use("/api/employee", employeeRoutes);
 app.use("/api/branches", branchesRoutes);
 app.use("/api/reviews", reviewsRoutes);
-b;
+
 
 app.use("/api/registerEmployees", registerEmployeesRoutes);
 app.use("/api/login", loginRoutes);
